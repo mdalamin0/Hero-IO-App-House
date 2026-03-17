aa93bbe64e195eccede1cd625a4fcec5398d729a
Al-amin <muhammadalamin809@gmail.com>
2026-03-17 16:05:19 +0800

    Project setup done

---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+  return (
+    <div>
+      hellow
+    </div>
+  );
+};
+
+export default Root;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Pages/Root/Root";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>
+  }
+])
+
