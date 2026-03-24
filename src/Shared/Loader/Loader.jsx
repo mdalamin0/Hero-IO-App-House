c3ed4ef027e81d7036dbefd396da5a3b140bc651
Al-amin <muhammadalamin809@gmail.com>
2026-03-24 17:43:36 +0800

    redirects page added

--- src/Shared/Loader/Loader.jsx
+++ src/Shared/Loader/Loader.jsx
@@ -6,9 +6,9 @@ const Loader = ({ isExiting }) => {
     <div className={`flex flex-col items-center justify-center h-screen bg-base-300 transition-all duration-700 ease-in-out ${isExiting ? 'scale-150 opacity-0' : 'scale-100 opacity-100'
       }`}>
       <div className='flex items-center gap-3'>
-        <h1 className='text-6xl font-bold text-gray-700'>L</h1>
+        <h1 className='text-6xl font-bold text-gray-700 dark:text-gray-400'>L</h1>
         <img src={LoadingImg} alt="logo" className='w-16 h-16 animate-spin' />
-        <h1 className='text-6xl font-bold text-gray-700'>ading</h1>
+        <h1 className='text-6xl font-bold text-gray-700 dark:text-gray-400'>ading</h1>
       </div>
     </div>
   );
