6d1a3764fe69e6fa3c219b8ddf026e9791f6156f
Al-amin <muhammadalamin809@gmail.com>
2026-03-25 10:39:44 +0800

    error page responsive bug fix

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 const ErrorPage = () => {
   return (
-    <div className='flex flex-col justify-center items-center h-screen space-y-4 text-center py-5 px-4'>
+    <div className='flex flex-col justify-center items-center h-screen space-y-4 text-center px-4'>
       <img src={ErrorImg} alt="Error Image" />
       <h2 className='text-3xl md:text-5xl font-semibold'>Oops, page not found!</h2>
       <p className='text-gray-600 dark:text-gray-400 md:text-xl'>Sorry, the page you are looking for might be removed, renamed, or is temporarily unavailable.</p>
